7816797f28978b7677e1ff0f7fe9f5750c859307
AndreFSilveira <andrefelipesilveira@gmail.com>
2013-11-09 19:37:08 -0200

    envio da primeira pagina terminal  html css e javascript

---
+++ integrador.js
@@ -0,0 +1,22 @@
+function comando(){
+	var novocampo = document.getElementById("novocampo");
+	var mensagem;
+	if( event.keyCode == 13)
+	{
+		if((document.getElementById("dados").value=="ls") || (document.getElementById("dados").value=="LS" )) 
+			mensagem="<a href='desktop.html'>Desktop</a>";
+		else if((document.getElementById("dados").value=="clear") || (document.getElementById("dados").value=="CLEAR" )) 
+			window.location.reload(); 
+		else if (document.getElementById("dados").value=='NULL')
+			mensagem=" ";
+		else
+			mensagem="comando não encontrado.";
+		document.body.onkeypress = comando;
+		novocampo.innerHTML = mensagem;
+	}
+
+}
+function SetFocus(elementToFocus)
+{
+	document.getElementById(elementToFocus).focus();
+}
